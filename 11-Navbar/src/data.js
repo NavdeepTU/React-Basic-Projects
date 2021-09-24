eb543c5adc326da5e384d384ea1120ee1ab94bf8
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-09-24 08:06:17 +0530

    moved links from component to a seperate js file in an array instead of hardcoding every time

--- 11-Navbar/src/data.js
+++ 11-Navbar/src/data.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa'
-export const links = [
-  {
+import {
+  FaBehance,
+  FaFacebook,
+  FaLinkedin,
+  FaTwitter
+} from 'react-icons/fa'
+export const links = [{
     id: 1,
     url: '/',
-    text: 'random',
+    text: 'home',
   },
   {
     id: 2,
@@ -28,25 +32,24 @@ export const links = [
   },
 ]
 
-export const social = [
-  {
+export const social = [{
     id: 1,
-    url: 'https://www.twitter.com',
-    icon: <FaFacebook />,
+    url: 'https://www.facebook.com',
+    icon: < FaFacebook / > ,
   },
   {
     id: 2,
     url: 'https://www.twitter.com',
-    icon: <FaTwitter />,
+    icon: < FaTwitter / > ,
   },
   {
     id: 3,
-    url: 'https://www.twitter.com',
-    icon: <FaLinkedin />,
+    url: 'https://www.linkedin.com',
+    icon: < FaLinkedin / > ,
   },
   {
     id: 4,
-    url: 'https://www.twitter.com',
-    icon: <FaBehance />,
+    url: 'https://www.behance.net/',
+    icon: < FaBehance / > ,
   },
 ]
