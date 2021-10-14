4d85b575cc304caf800870b7559be811acd458a6
Navdeep Sharma <navdeepsharmatu@gmail.com>
2021-10-14 14:02:45 +0530

    added bussiness logic for closing the submenu

--- 13-Stripe-Submenus/src/Navbar.js
+++ 13-Stripe-Submenus/src/Navbar.js
@@ -5,6 +5,7 @@ import { useGlobalContext } from './context';
 
 const Navbar = () => {
   const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+
   const displaySubmenu = (e) => {
     const page = e.target.textContent;
     const tempBtn = e.target.getBoundingClientRect();
@@ -12,8 +13,15 @@ const Navbar = () => {
     const bottom = tempBtn.bottom - 3;
     openSubmenu(page, { center, bottom });
   };
+
+  const handleSubmenu = (e) => {
+    if (!e.target.classList.contains('link-btn')) {
+      closeSubmenu();
+    }
+  };
+
   return (
-    <nav className="nav">
+    <nav className="nav" onMouseOver={handleSubmenu}>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className="nav-logo" alt="stripe" />
